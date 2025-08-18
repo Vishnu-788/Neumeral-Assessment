7fe644031b2b228e75e71bcc77de8cd1973f1e05
Vishnu Prasad <prasadvishnu788@gmail.com>
2025-08-18 17:45:24 +0530

    Created -> Client Module Skeleton created using vite. --> Remvoed all unecessary designs and everything. Starting fresh.

---
+++ client/src/utils/api.js
@@ -0,0 +1,10 @@
+export const BASE_URL = "http://127.0.0.1:8000/api/v1";
+
+export const API_ENDPOINTS = {
+  SIGN_IN: `${BASE_URL}/user/login/`,
+  SIGN_UP: `${BASE_URL}/user/register/`,
+  SIGN_OUT: `${BASE_URL}/user/logout/`,
+  PROFILE: `${BASE_URL}/user/profile/`,
+  BOOK_APPOINTMENT: `${BASE_URL}/appointment/create/`,
+  GET_APPOINTMENTS: `${BASE_URL}/appointment/`,
+};
